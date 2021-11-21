37c78a93f95ac76636546c4540aaf5e89f5aad9b
jozefboris <borisjozef@gmail.com>
2021-11-21 19:57:15 +0100

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -35,7 +35,8 @@ import { ShortenPipe } from './pipes/shorten.pipe';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/tunes/tunes-song/tunes-song.component.ts
+++ src/app/components/tunes/tunes-song/tunes-song.component.ts
@@ -1,10 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { negate } from 'lodash';
 import { Song } from 'src/app/types';
+import { trigger, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'tunes-song',
   templateUrl: './tunes-song.component.html',
-  styleUrls: ['./tunes-song.component.scss']
+  styleUrls: ['./tunes-song.component.scss'],
+  animations: [
+		trigger('songAnim', [
+			transition(':enter', [
+				style({
+					opacity: 0,
+					transform: 'scale(0.85)'
+				}),
+				animate('0.2s ease')
+			]),
+			transition(':leave', [
+				animate(
+					'0.2s ease',
+					style({
+						opacity: 0,
+						transform: 'scale(0.85)'
+					})
+				)
+			])
+		])
+	]
 })
 export class TunesSongComponent implements OnInit {
 
